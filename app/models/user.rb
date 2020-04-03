289dd791697069d3149b86cefbdb0e674fc29b01
Arai-Mami <61001274+Arai-Mami@users.noreply.github.com>
2020-04-03 16:16:55 +0900

    user.rbにvalidates追記
    モデルレベルでのバリデーションを設定するために、uesr.rbにvalidatesを追記。
    databese.ymlのエンコードを編集。（stringの文字制限を編集しなくてもこれで済んでいたはず...
    )

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true, uniqueness: true
+  #nameカラムが、空の場合はfalse、uniqueで、同じ値が保存できないようにする
+  #Railsチームはモデルレベルでのバリデーションが最も適切であると考えている。
+  #他にも、クライアント側でのバリデーション、データベース制約（データベース側でのバリデーション）、コントローラーでのバリデーションなどもある
 end
