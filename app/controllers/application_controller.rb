cdfd415b5008cb4ab452a61d20f30d8ebef168b4
Arai-Mami <61001274+Arai-Mami@users.noreply.github.com>
2020-04-03 17:50:51 +0900

    deviseにnameのパラメーター追加
    カリキュラムに沿って記述。devise経由で作られたusersテーブルにはもともとnameないけれど、nameカラムを追加したので、sign_up時のパラメーターを追加。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  protect_form_forgey with: :exception
+  #CSRF対策
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    #deviseにnameを追加したため、deviseのパラメーターにも追加する
+    #sign_upのとき
+  end
 end
