330e7de19ee032d8a80028db3ec39aa19723d51b
Arai-Mami <61001274+Arai-Mami@users.noreply.github.com>
2020-04-07 20:44:35 +0900

    トップページにアクセスした際、サイドバーのみ表示されるように設定する
    routes.rbのrootパスの記述を変更。
    root "groups#index"

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: [:edit, :update]
   resources :groups, only: [:new, :create, :edit, :update]
-  root "messages#index"
+  root "groups#index"
+  #トップページのアクセスでchatのメイン部分は表示せず、side_barのみ表示するようにする
 end
