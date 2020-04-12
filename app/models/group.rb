fb7fd8afec28ec0ea2ce64df365c9913b279f659
Arai-Mami <61001274+Arai-Mami@users.noreply.github.com>
2020-04-12 11:58:21 +0900

    UserGroupsとGroupsのクラスの継承を変更
    ApplicationRecordだとエラーが出るので、ActiveRecord::Base

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,4 @@
-class Group < ApplicationRecord
+class Group < ActiveRecord::Base
   has_many :group_users
   has_many :users, through: :group_users
   validates :group_name, presence: true, uniqueness: true
--- app/models/group_user.rb
+++ app/models/group_user.rb
@@ -1,4 +1,4 @@
-class GroupUser < ApplicationRecord
+class GroupUser < ActiveRecord::Base
   belongs_to :user
   belongs_to :group
 end
