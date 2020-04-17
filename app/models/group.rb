e818496bf76c5478487e36236f9da902b3a84968
Arai-Mami <61001274+Arai-Mami@users.noreply.github.com>
2020-04-17 14:36:59 +0900

    show_last_messageメソッドをGroupモデルに定義

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,4 +3,16 @@ class Group < ActiveRecord::Base
   has_many :users, through: :group_users
   has_many :messages
   validates :group_name, presence: true, uniqueness: true
+
+  def show_last_message
+    if(last_message.last).present?
+      if last_message.content?
+        last_message.content
+      else
+        '画像が投稿されています'
+      end
+    else
+      'まだメッセージはありません'
+    end
+  end
 end
