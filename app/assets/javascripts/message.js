47e9591b082bfeb006fb161d2b77c6b4d05f1e7c
Arai-Mami <61001274+Arai-Mami@users.noreply.github.com>
2020-05-13 01:27:05 +0900

    JSONで返すことに成功！ボタンも連続して押せる様になった！

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,10 +1,48 @@
 $(function(){
+  function buildHTML(message){
+    if ( message.image){
+      let html =
+      `<div class="message-box">
+        <div class="message-info">
+          <div class="message-info__user-name">
+            ${message.user_name}
+            <div class="message-info__date">
+              ${message.created_at}
+              <div class="message-text">
+              ${message.message}
+              </div>
+              <img class="message__image" src="${message.image}">
+            </div>
+          </div>
+        </div>
+      </div>`
+      return html;
+    }else {
+      let html =
+      `<div class="message-box">
+        <div class="message-info">
+          <div class="message-info__user-name">
+            ${message.user_name}
+            <div class="message-info__date">
+              ${message.created_at}
+              <div class="message-text">
+              ${message.message}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>`
+      return html;
+    };
+  }
   $('.Form').on('submit', function(e){
     // Formに関しては検証ツールを確認するとわかる！submitというイベントに発火している
     e.preventDefault()
     // デフォルトのフォーム送信というアクションはされなくなっている
     let formData = new FormData(this);
+    console.log(formData)
     let url = $(this).attr('action');
+    console.log(url)
     // $(this)はonメソッドの中で使うと、onメソッドを利用しているノードのオブジェクトが参照される
     // つまり今回はform要素自体
     $.ajax({
@@ -14,6 +52,17 @@ $(function(){
       dataType: 'json',
       processData: false,
       contentType: false,
+    })
+    .done(function(data){
+      console.log(data)
+      let html = buildHTML(data);
+      console.log(html)
+      $('.message-list').append(html)
+      $('.message-list').animate({ scrollTop: $('.message-list')[0].scrollHeight});
+      //#下までスクロール
+      $('Form')[0].reset();
+      submit=$('.send-btn').prop('disabled', false);
+      console.log(submit)
     })
   });
 });
