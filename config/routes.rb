8e031fbea55160769bf712eba460cce74cbc8002
Arai-Mami <61001274+Arai-Mami@users.noreply.github.com>
2020-05-15 12:32:28 +0900

    元通り？

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   get 'messages/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users, only: [:edit, :update]
+  resources :users, only: [:index, :edit, :update]
   resources :groups do
     resources :messages, only: [:index, :create]
   end
