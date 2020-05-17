23a0abf002af5dd4e0ee5f0a1683e04f91014986
Arai-Mami <61001274+Arai-Mami@users.noreply.github.com>
2020-05-17 14:47:20 +0900

    viewフォルダの中にapiフォルダ、その中にmessagesフォルダを作成し、index.json.jbuilderファイルを作成

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: [:index, :edit, :update]
   resources :groups do
+    #ルーティングをネスト
     resources :messages, only: [:index, :create]
+  
+    namespace :api do
+      #namespaceを利用すると、そのディレクトリ内のアクションを指定することができる
+      resources :messages, only: :index, defaults: { format: 'json' }
+    end
   end
   root "groups#index"
   #トップページのアクセスでchatのメイン部分は表示せず、side_barのみ表示するようにする
