a162302e4fff840627bc8dbc29097705bfd1bca2
Arai-Mami <61001274+Arai-Mami@users.noreply.github.com>
2020-05-18 00:02:15 +0900

    自動更新機能追加できた！

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'messages/index'
+  root "groups#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: [:index, :edit, :update]
-  resources :groups do
+  resources :groups, only: [:new, :create, :edit, :update] do
     #ルーティングをネスト
     resources :messages, only: [:index, :create]
-  
     namespace :api do
       #namespaceを利用すると、そのディレクトリ内のアクションを指定することができる
       resources :messages, only: :index, defaults: { format: 'json' }
     end
   end
-  root "groups#index"
   #トップページのアクセスでchatのメイン部分は表示せず、side_barのみ表示するようにする
 end
